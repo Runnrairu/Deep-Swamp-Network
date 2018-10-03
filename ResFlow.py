3dcd45fdccab479225570bc0124abfe7159e4670
konatsu <runnrairu@gmail.com>
2018-10-03 22:58:30 +0900

    Update ResFlow.py

--- ResFlow.py
+++ ResFlow.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+import tensorflow as tf
 
-def ResFlow(inpt,delta_t):
+
+def ResFlow(inpt,delta_t,delta_w):
     
+    f_x = Res_func(inpt)
+    
+    
+    return inpt+0.5*delta_t*f_x +0.025*delta_w*f_x
+   
+def Res_func(inpt):
+   
+   
+   
+   
+   
+   
+   return output
