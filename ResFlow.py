bb0eee0eaf3afc050b67a89d8cfe127365d1bce0
konatsu <runnrairu@gmail.com>
2018-10-09 19:03:34 +0900

    Update ResFlow.py

--- ResFlow.py
+++ ResFlow.py
@@ -10,8 +10,10 @@ def ResFlow(inpt,delta_t,delta_w):
     return inpt+0.5*delta_t*f_x +0.025*delta_w*f_x
    
 def Res_func(inpt):
-   
-   
+    W_conv1 = weight_variable([5, 5, 64, 64])
+    b_conv1 = bias_variable([64])
+    W_conv2 = weight_variable([5, 5, 64, 64])
+    b_conv2 = bias_variable([64])
    
    
    
