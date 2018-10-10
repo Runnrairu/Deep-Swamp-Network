f0a145cdaf467326e039e5fea0fef3b9e7b6bdac
konatsu <runnrairu@gmail.com>
2018-10-10 20:34:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import load_data
 import tensorflow as tf
 import numpy as np
 
-
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
 
 def run():
+    a
+    a
+    a
+    a
+    a
+    X_train, Y_train, X_test, Y_test = load_data.load()
+    
+    X = tf.placeholder("float", [batch_size, 32, 32, 3])
+    Y = tf.placeholder("float", [batch_size, 10])
+    learning_rate = tf.placeholder("float", [])
+    
+    
+    
+    
