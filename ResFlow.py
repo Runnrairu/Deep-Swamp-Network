79163a1b2096dd1e84ca2651790aa5c5d1d9b784
konatsu <runnrairu@gmail.com>
2018-10-10 22:24:08 +0900

    Update ResFlow.py

--- ResFlow.py
+++ ResFlow.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 
+def tW_def(n,name):
+    if name == "Fukasawa_scheme":
+        t,W = Fukasawa_scheme(1)
+    elif name == "Simplicity_scheme":
+        t,W = Simplicity_scheme(1)
+    elif name == "Euler_Maruyama_scheme":
+        t,W = Euler_Maruyama_scheme(n,1)
+    elif name == "Milstein_scheme":
+        t,W == Milstein_scheme(n,1)
+    else:
+        print("Invarid!")
+    return t,W
+
+def Fukasawa_scheme(T):
+    
+    return t,W
+
+def Simplicity_scheme(T):
+    
+    return t,W
+
+
+
+
+def Euler_Maruyama_scheme(n,T):
+    
+    return t,W
+
+
+def Milstein_scheme(n,T):
+    
+    retrun t,W
+
+
+
 
 def ResFlow(inpt,delta_t,delta_w):
     
