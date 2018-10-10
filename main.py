a990247fd4feb1495ee5faa259164e17dd79ba06
konatsu <runnrairu@gmail.com>
2018-10-10 22:13:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import load_data
 import tensorflow as tf
 import numpy as np
+import ResFlow as RF
 
 BATCH_SIZE = 64
 LEARNING_RATE = 1e-4
@@ -26,10 +27,13 @@ def run():
     saver = tf.train.Saver()
     for j in range (10):
         for i in range (0, 50000, batch_size):
+            t,W = RF.tW_def(Fukasawa_scheme)
             feed_dict={
                 X: X_train[i:i + batch_size], 
                 Y: Y_train[i:i + batch_size],
-                learning_rate: 0.001}
+                learning_rate: 0.001,
+                time_list:t,
+                W_list:W}
             sess.run([train_op], feed_dict=feed_dict)
             if i % 512 == 0:
                 print "training on image #%d" % i
@@ -39,8 +43,10 @@ def run():
         if i + batch_size < 10000:
             acc = sess.run([accuracy],feed_dict={
                 X: X_test[i:i+batch_size],
-                Y: Y_test[i:i+batch_size]
-            })
+                Y: Y_test[i:i+batch_size],
+                time_list:t,
+                W_list:W
+                })
             accuracy_summary = tf.scalar_summary("accuracy", accuracy)
             print acc
 
