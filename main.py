73dd5fa77fcbcafc34859c7cab2d81d4aadb4fed
konatsu <runnrairu@gmail.com>
2018-11-10 20:51:07 +0900

    Update main.py
    訓練パラメータ数を自動計算

--- main.py
+++ main.py
@@ -137,8 +137,23 @@ def run():
                 print("saved!")
             print( "accuracy after epoch %d : %.3f " % (j,sess.run(accuracy,feed_dict=feed_dict_test) ))
            # accuracy_summary = tf.scalar_summary("accuracy", accuracy)
+    #ここからパラメータ数計算および列挙
+    total_parameters = 0
+    parameters_string = ""
+    for variable in tf.trainable_variables():
 
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim.value
+        total_parameters += variable_parameters
+        if len(shape) == 1:
+            parameters_string += ("%s %d, " % (variable.name, variable_parameters))
+        else:
+            parameters_string += ("%s %s=%d, " % (variable.name, str(shape), variable_parameters))
 
+    print(parameters_string)
+    print("Total %d variables, %s params" % (len(tf.trainable_variables()), "{:,}".format(total_parameters)))
     sess.close()
 import time
 if __name__ == '__main__':
